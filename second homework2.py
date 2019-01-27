a2a98bda65bd2d8c16cb14938a315054086c65ef
Sushmita00 <46702078+Sushmita00@users.noreply.github.com>
2019-01-27 22:52:09 +0545

    Add files via upload

---
+++ second homework2.py
@@ -0,0 +1,3 @@
+list=[0,6,8,23,56,78,98]
+print("maximum value is:",max(list))
+print("minimum value is:",min(list))
---
+++ second homework3.py
@@ -0,0 +1,4 @@
+name="man is a social animal"
+replace=name.replace('man','human')
+print(replace)
+print(name.upper())
